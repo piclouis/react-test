0770172133339b174055f20f26943f9881a21263
Louis-Philip Picard <louis-philip.picard@anekdotes.com>
2022-08-02 16:23:17 -0400

    navbar

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,53 +2,23 @@ import React, { Component } from 'react';
 import Counter from './counter';
 
 class Counters extends Component {
-  state = { 
-    counters: [
-      { id:1 , value: 4},
-      { id:2 , value: 5},
-      { id:3 , value: 0},
-      { id:4 , value: 0}
-    ]
-  };
-
-  handleIncrement = counterId => {
-    const counters = this.state.counters.map(c => {
-      if (c.id === counterId) {
-        ++c.value;
-      }
-      return c;
-    });
-    this.setState({counters});
-  }
-
-  handleDelete = counterId => {
-    const counters = this.state.counters.filter(c => c.id !== counterId);
-    this.setState({counters});
-  }
-
-  handleReset = () => {
-    const counters = this.state.counters.map(c => {
-      c.value = 0
-      return c;
-    });
-
-    this.setState({counters});
-  }
-
+  
   render() { 
+    const { onReset, onDelete, onIncrement, counters } = this.props;
+
     return (
       <div>
         <button 
-          onClick={this.handleReset}
+          onClick={onReset}
           className="btn btn-sm m-2"
         >
           Reset  
         </button>
-        {this.state.counters.map( counter => (
+        {counters.map( counter => (
           <Counter 
             key={counter.id} 
-            onDelete={this.handleDelete} 
-            onIncrement={this.handleIncrement} 
+            onDelete={onDelete} 
+            onIncrement={onIncrement} 
             counter={counter}
           />
         ))}
---
+++ src/components/navbar.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+const NavBar = ({totalCounters}) => {
+  return (
+    <nav className="navbar bg-light">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          Navbar
+          <span className="badge badge-pill badge-secondary m-2">{totalCounters}</span>
+        </a>
+      </div>
+    </nav>
+  );
+}
+ 
+export default NavBar;
